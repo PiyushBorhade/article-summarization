fd2fb227042b459ea519c09ce808502f0d8b9441
Piyush Borhade <68846327+PiyushBorhade@users.noreply.github.com>
2025-03-27 00:20:47 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from groq import Groq
 from PIL import Image
 
 # Initialize the Groq client
-client = Groq(api_key='Groq_API_KEY')
+#st.secrets["OPENAI_API_KEY"]
+client = st.secrets(api_key='Groq_API_KEY')
 
 def extract_text_from_pdf(file):
     doc = fitz.open(stream=file.read(), filetype="pdf")  # Open PDF from file object
